90f0f8b0c028a65c3cab39841317f9322f6397d0
ashir <ashir@metrictreelabs.com>
2024-02-26 11:03:08 +0530

    initial server setup done

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("jiiii")
+import express, { Express } from "express";
+import cors from "cors";
+import { port } from "../config/constants";
+import morgan from "morgan";
+import fs from "fs";
+import path from "path";
+const app: Express = express();
+var accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), {
+  flags: "a",
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("combined", { stream: accessLogStream }));
+
+
+
+app.listen(port, () => console.log("listening on port " + port));
