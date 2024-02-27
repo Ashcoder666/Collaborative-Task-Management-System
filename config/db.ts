5ce4e815b48cc2703e359274ceb7911cf70a63e0
ashir <ashir@metrictreelabs.com>
2024-02-27 09:42:00 +0530

    feb 26

---
+++ config/db.ts
@@ -0,0 +1,18 @@
+const { Sequelize } = require('sequelize');
+
+
+const sequelize = new Sequelize('postgres://postgres:1234@localhost:5432/ctmdb') // Example for postgres
+
+// Option 2: Passing parameters separately (sqlite)
+// const sequelize = new Sequelize({
+//   dialect: 'sqlite',
+//   storage: 'path/to/database.sqlite'
+// });
+
+// // Option 3: Passing parameters separately (other dialects)
+// const sequelize = new Sequelize('database', 'username', 'password', {
+//   host: 'localhost',
+//   dialect: /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+// });
+
+export default sequelize
